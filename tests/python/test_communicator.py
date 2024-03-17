d1db3a6007ffb97c9a8c430c17ad5915ce3c93c5
Matthieu Dreher <dreher.matthieu@gmail.com>
2024-03-16 23:52:30 -0400

    Add unit test for communicator for coverage testing.

---
+++ tests/python/test_communicator.py
@@ -0,0 +1,41 @@
+from godrick.workflow import Workflow
+from godrick.task import MPITask, MPIPlacementPolicy
+from godrick.launcher import MainLauncher
+from godrick.computeResources import ComputeCollection
+from godrick.communicator import MPICommunicator, MPICommunicatorProtocol
+
+import os
+from pathlib import Path
+
+def test_MPICommunicator():
+    exampleFile = os.path.join(Path(__file__).resolve().parent, "../../data/tests/singlehost.txt")
+    cluster = ComputeCollection(name="myCluster")
+    cluster.initFromHostFile(exampleFile, True)
+
+    workflow = Workflow("MPIBroadcastWorkflow")
+    partitions = cluster.splitNodesByCoreRange([1, 3])
+
+    task1 = MPITask(name="send", cmdline="bin/send --name send --config config.MPIBroadcastWorkflow.json", placementPolicy=MPIPlacementPolicy.ONETASKPERCORE)
+    task1.addOutputPort("out")
+    task1.setResources(partitions[0])
+    task2 = MPITask(name="receive", cmdline="bin/receive --name receive --config config.MPIBroadcastWorkflow.json", placementPolicy=MPIPlacementPolicy.ONETASKPERCORE)
+    task2.addInputPort("in")
+    task2.setResources(partitions[1])
+
+    comm1 = MPICommunicator("myComm")
+    comm1.connectInput(task2.getInputPort("in"))
+    comm1.connectOutput(task1.getOutputPort("out"))
+
+    workflow.declareTask(task1)
+    workflow.declareTask(task2)
+    workflow.declareCommunicator(comm1)
+
+    launcher = MainLauncher()
+    launcher.generateOutputFiles(workflow=workflow)
+
+    configFile = Path(workflow.getConfigurationFile())
+    assert configFile.is_file()
+    with open(configFile) as f:
+        doc = f.read()
+        print(doc)
+    raise RuntimeError()
